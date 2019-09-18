5cf3adf6ce478369ca6980134b313e9117ca500e
Luis Contreras <luis.contreras@globant.com>
2019-09-18 00:32:36 -0500

    script borrado 2

--- borrarlogs.sh
+++ borrarlogs.sh
@@ -2,4 +2,3 @@ find /siebel/ses/siebsrvr/enterprises/SBA_82/sadbogca629/log/ -name "*.log" -mti
 find /siebel/ses/siebsrvr/enterprises/SBA_82/sadbogca629/logarchive/ -name "archive*" -mtime +15 -print -exec rm -rf {} \;
 find /siebel/sai/applicationcontainer/logs \( -name "*.txt*" -o -name "*.log*" \) -mtime +5 -print -exec rm {} \;
 find /siebel/ses/applicationcontainer/logs \( -name "*.txt*" -o -name "*.log*" \) -mtime +5 -print -exec rm {} \;
-#oprueba
