7dfd69f5e898c450005ff32ef10e617f999d6aa0
Luis Contreras <lcontreras48@gmail.com>
2019-10-01 16:10:54 -0500

    update borralogs v2.4

--- borrarlogs.sh
+++ borrarlogs.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
+FIND=$(which find)
 TAR=$(which tar)
 DATE=$(which date)
 GZIP=$(which gzip)
 PATHTAR="/home/siebel/tar"
 HOST=$HOSTNAME
-echo $HOST
 SERVER=$(echo $HOST |cut -d'.' -f1)
-echo $SERVER
 PATHLOG="/siebel/ses/siebsrvr/enterprises/SBA_82/$SERVER/log"
 PATHARCHIVE="/siebel/ses/siebsrvr/enterprises/SBA_82/$SERVER/logarchive"
-echo $PATHLOG
-find $PATHLOG \( -name "SBA*" -o -name "EAI*.log" -o -name "FINS*" \)  -mtime +5 -print -exec | $XARGS $TAR -cvf $PATHTAR/backup_eai_fins_logs-$($DATE +%Y%m%d).tar \;
-$GZIP $PATHTAR/backup_eai_fins_logs-$($DATE +%Y%m%d).tar
-find $PATHLOG -name "*.log"  -mtime +5 -print -exec rm -rf {} \;
+find $PATHLOG \( -name "SBA*" -o -name "EAI*.log" -o -name "FINS*" \)  -mtime +5 -print -exec $TAR rvf $PATHTAR/backup_eai_fins_logs-$($DATE +%Y%m%d).tar {} \;
+find $PATHLOG "*.log" -mtime +5 -print -exec rm -rf {} \;
 $GZIP $PATHTAR/backup_eai_fins_logs-$($DATE +%Y%m%d).tar
 find $PATHARCHIVE -name "archive*" -mtime +15 -print -exec rm -rf {} \;
 find /siebel/sai/applicationcontainer/logs \( -name "*.txt*" -o -name "*.log*" \) -mtime +5 -print -exec rm {} \;
