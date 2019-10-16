6b8b7dccbeb3daf4bcc86a31d9f165956ff4287b
Luis Contreras <luis.contreras@globant.com>
2019-10-15 20:41:42 -0500

    borrar fdr v4

--- borrarfdr.sh
+++ borrarfdr.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-FIND=$(which find)
-XARGS=$(which xargs)
-TAR=$(which tar)
 DATE=$(which date)
-GZIP=$(which gzip)
 PATHFDR="/siebel/ses/siebsrvr/bin/fdr"
 PATHTAR="/home/siebel/tar"
-echo $(FIND $PATHFDR \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -type f -mtime +0) | $XARGS tar -cvf $PATHTAR/backup-$($DATE +%Y%m%d).tar
+echo $(FIND $PATHFDR \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -type f -mtime +0) | xargs tar -cvf $PATHTAR/backup-$($DATE +%Y%m%d).tar
 gzip $PATHTAR/backup-$($DATE +%Y%m%d).tar
 find $PATHFDR -type f \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -mtime +0 -delete
 find $PATHTAR -type f -name "*.tar.gz" -mtime +5 -delete
