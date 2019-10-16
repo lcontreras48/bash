343aa31e92f4f0bf67eb982581c7bf359e38ba48
Luis Contreras <luis.contreras@globant.com>
2019-10-15 20:41:00 -0500

    borrar fdr v3

--- borrarfdr.sh
+++ borrarfdr.sh
@@ -4,9 +4,9 @@ XARGS=$(which xargs)
 TAR=$(which tar)
 DATE=$(which date)
 GZIP=$(which gzip)
-PATH="/siebel/ses/siebsrvr/bin/fdr"
+PATHFDR="/siebel/ses/siebsrvr/bin/fdr"
 PATHTAR="/home/siebel/tar"
-echo $($FIND $PATH \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -type f -mtime +0)   | $XARGS $TAR -cvf $PATHTAR/backup-$($DATE +%Y%m%d).tar
-$GZIP $PATHTAR/backup-$($DATE +%Y%m%d).tar
-$FIND $PATH -type f \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -mtime +0 -delete
-$FIND $PATHTAR -type f -name "*.tar.gz" -mtime +30 -delete
+echo $(FIND $PATHFDR \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -type f -mtime +0) | $XARGS tar -cvf $PATHTAR/backup-$($DATE +%Y%m%d).tar
+gzip $PATHTAR/backup-$($DATE +%Y%m%d).tar
+find $PATHFDR -type f \( -name "core*" -o -name "*.fdr" -o -name "callstack*" \) -mtime +0 -delete
+find $PATHTAR -type f -name "*.tar.gz" -mtime +5 -delete
