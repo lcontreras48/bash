586893781fc05dbd5d3e3e66bce79317048ff667
Luis Contreras <luis.contreras@globant.com>
2019-10-17 01:53:33 -0500

    Borrar logs v5

--- borrarlogs.sh
+++ borrarlogs.sh
@@ -8,7 +8,7 @@ PATHARCHIVE="/siebel/ses/siebsrvr/enterprises/SBA_82/$SERVER/logarchive"
 find $PATHLOG \( -name "SBA*" -o -name "EAI*.log" -o -name "FINS*" \)  -mtime +0 -exec tar rvf $PATHTAR/backup_eai_fins_logs-$($DATE +%Y%m%d).tar {} \;
 find $PATHLOG "*.log" -mtime +0 -exec rm -rf {} \;
 gzip $PATHTAR/backup_eai_fins_logs-$($DATE +%Y%m%d).tar
-find $PATHARCHIVE -name "archive*" -mtime +15 -print -exec rm -rf {} \;
+find $PATHARCHIVE -name "archive*" -mtime +4 -print -exec rm -rf {} \;
 find /siebel/sai/applicationcontainer/logs \( -name "*.txt*" -o -name "*.log*" \) -mtime +0 -exec rm {} \;
 find /siebel/ses/applicationcontainer/logs \( -name "*.txt*" -o -name "*.log*" \) -mtime +0 -exec rm {} \;
 find /siebel/ses/siebsrvr/log \( -name "*.dmp" -o -name "*.txt*" -o -name "*.log*" \) -mtime +0 -exec rm {} \;
